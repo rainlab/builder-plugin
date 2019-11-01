f2de970adbcb31c6fae0aa659ddfc5a28cb36c49
Ben Thomson <git@alfreido.com>
2019-11-01 09:07:11 +0800

    Update lang.php

--- lang/fa/lang.php
+++ lang/fa/lang.php
@@ -195,7 +195,7 @@ return [
         'property_datepicker_min_date' => 'کمترین تاریخ',
         'property_datepicker_min_date_description' => 'The minimum/earliest date that can be selected. This may be any string accepted by Carbon. Leave empty for no minimum date.',
         'property_datepicker_max_date' => 'بیشترین تاریخ',
-        'property_datepicker_max_date_description' => 'The maximum/latest date that can be selected. This may be any string accepted by Carbon. Leave empty for no maximum date.',        'property_markdown_mode' => 'نحوه نمایش',
+        'property_datepicker_max_date_description' => 'The maximum/latest date that can be selected. This may be any string accepted by Carbon. Leave empty for no maximum date.',
         'property_markdown_mode' => 'نحوه نمایش',
         'property_markdown_mode_split' => 'پنجره کنار هم',
         'property_markdown_mode_tab' => 'Tab',
